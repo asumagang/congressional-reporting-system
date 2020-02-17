fe9fec4836f6235b6b5b0e93e6237b2257db6cb3
asumagang <janaidrhean@gmail.com>
2020-02-17 14:46:15 +0800

    pofffffff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { AuthGuard } from "./_helpers/auth.guard";
 import { authInterceptorProviders } from "./_helpers/auth.interceptor";
 import { AppComponent } from "./app.component";
 import { SidebarComponent } from "./sidebar/sidebar.component";
-import { AdminLayoutComponent } from "./admin-layout/admin-layout.component";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
 import { NavbarComponent } from "./navbar/navbar.component";
