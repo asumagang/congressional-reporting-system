a0eaac7bd83aa90f502422c4c7d0dd09e51052bd
kar-ren <karxi0131@gmail.com>
2020-02-26 22:39:01 +0800

    profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -14,7 +14,6 @@ export class ProfileComponent implements OnInit {
   constructor(
     private token: TokenStorageService,
     private modalService: NgbModal,
-    private httpClient: HttpClient
     ) { }
 
   ngOnInit() {
@@ -27,47 +26,5 @@ export class ProfileComponent implements OnInit {
   }
 
   
-  title = 'ImageUploaderFrontEnd';
-
-  public selectedFile;
-  public event1;
-  imgURL: any;
-  receivedImageData: any;
-  base64Data: any;
-  convertedImage: any;
-
-  public  onFileChanged(event) {
-    console.log(event);
-    this.selectedFile = event.target.files[0];
-
-    // Below part is used to display the selected image
-    let reader = new FileReader();
-    reader.readAsDataURL(event.target.files[0]);
-    reader.onload = (event2) => {
-      this.imgURL = reader.result;
-  };
-
- }
-
-
- // This part is for uploading
- onUpload() {
-
-
-  const uploadData = new FormData();
-  uploadData.append('myFile', this.selectedFile, this.selectedFile.name);
-
-
-  this.httpClient.post('http://localhost:8080/api/test/profileimage/upload', uploadData)
-  .subscribe(
-               res => {console.log(res);
-                       this.receivedImageData = res;
-                       this.base64Data = this.receivedImageData.pic;
-                       this.convertedImage = 'data:image/jpeg;base64,' + this.base64Data; },
-               err => console.log('Error Occured duringng saving: ' + err)
-            );
-
-
- }
 
 }
