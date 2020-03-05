b79088774e54cfbd65a738fa24c2adc612a1cbd8
kar-ren <karxi0131@gmail.com>
2020-03-05 23:17:59 +0800

    ahay

--- src/app/manageusers/activeusers/edituser/edituser.component.ts
+++ src/app/manageusers/activeusers/edituser/edituser.component.ts
@@ -9,52 +9,33 @@ import { User } from 'src/app/_models/user';
   styleUrls: ['./edituser.component.css']
 })
 export class EdituserComponent implements OnInit {
+  @Input()
+  user: User = {
+    id: 1,
+    firstname: "Adrian ",
+    lastname:" Sumagang",
+    mobileno:"09967812345",
+    username:"asumagang",
+    email:"adrian@gmail.com",
+    password:"asddddf",
+    address:"cebu city"
+  };
+  newFirstname: string;
+ 
 
-  // @Input()
-  // user: User = {
-  //   id: 1,
-  //   firstname: "Tanjiro ",
-  //   lastname: " Kamado",
-  //   username: " tkamado",
-  //   email: "tkamado@gmail.com",
-  //   mobileno:"09555187007",
-  //   address:"talamban,Cebu",
-  //   password:"12345678"
-  // };
-  // newFirstname: string;
-  // newLastname: string;
-  // newUsername: string;
-  // newMobileno:string;
-  // newAddress:string;
-  // newPassword:string;
-
-  // headTitle: string;
-
-  
+  headTitle: string;
   constructor(public activeModal: NgbActiveModal) { }
 
-  ngOnInit(){
-    
+  ngOnInit() {
+    this.headTitle = this.user ? "Edit User" : "Create User";
+    this.newFirstname = this.user ? this.user.firstname : "";
   }
-  // ngOnInit() {
-  //   this.headTitle = this.user ? "Edit User Details" : "Add New User";
-  //   this.newFirstname = this.user ? this.user.firstname : "";
-  //   this.newLastname = this.user ? this.user.lastname : "";
-  //   this.newAddress = this.user ? this.user.address : "";
-  //   this.newUsername = this.user ? this.user.username : "";
-  //   this.newMobileno = this.user ? this.user.mobileno: "";
-  //   this.newPassword = this.user ? this.user.password:"";
-  // }
   // submit() {
   //   if (this.user) {
   //     //Update program
   //     const updateUser: User = {
   //       id: this.user.id,
   //       firstname: this.newFirstname,
-  //       lastname: this.newLastname,
-  //       username: this.newUsername,
-  //       address: this.newAddress,
-  //       mobileno:this.newMobileno
   //     };
   //     this.activeModal.close(updateUser);
   //   } else {
@@ -62,11 +43,6 @@ export class EdituserComponent implements OnInit {
   //     const newUser: User = {
   //       id:null ,
   //       firstname: this.newFirstname,
-  //       lastname: this.newLastname,
-  //       username: this.newUsername,
-  //       address: this.newAddress,
-  //       mobileno: this.newMobileno,
-  //       password:this.newPassword,
   //     };
   //     this.activeModal.close(newUser);
   //   }
